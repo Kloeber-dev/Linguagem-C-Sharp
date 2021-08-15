38bf26e560da461e0df9608abd156f37349657e2
Kleber <klebersouza45@gmail.com>
2021-08-14 22:58:45 -0300

    upload Space Aulas

---
+++ Aulas/Aulas/Casa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aulas
+{
+    public class Casa
+    {
+        private float area;
+        private float altura;
+        private decimal preco;
+    }
+}
---
+++ Aulas/Aulas/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aulas
+{
+    public class Pessoa
+    {
+        private string nome;
+        private string sobrenome;
+        private byte idade;
+        private float altura;
+        private float peso;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+    }
+}
