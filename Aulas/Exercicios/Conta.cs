6164421523067e7ab21947f8e5632455ef6b8321
Kleber <klebersouza45@gmail.com>
2021-08-24 19:50:44 -0300

    upload Exercício2

---
+++ Aulas/Exercicios/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicios
+{
+    public class Conta
+    {
+        private string agencia;
+        private int numero;
+        private float saldo;
+
+        public void setAgencia(string agencia)
+        {
+            this.agencia = agencia;
+        }
+
+        public string getAgencia()
+        {
+            return agencia;
+        }
+
+        public void setNumero(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public int getNumero()
+        {
+            return numero;
+        }
+
+        public void setSaldo(float saldo)
+        {
+            this.saldo = saldo;
+        }
+
+        public float getSaldo()
+        {
+            return saldo;
+        }
+    }
+}
